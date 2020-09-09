c5c889270aa52a6ebdd4363bfa7afee0ba5317e4
Christie Yoon <cleex1999@gmail.com>
2020-09-09 23:15:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  get '/piglatinize' do
+     piglatin = PigLatinizer.new
+    @piglatinize = piglatin.piglatinize(params[:phrase])
+    erb :piglatinize
+  end
 end
